d078f84c5cf3cb50e5269228a670c9f5d78ae378
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2018-06-16 13:05:26 +0200

    Fixed an issue when other user already had a table named "logi".

--- logi.sql
+++ logi.sql
@@ -3,7 +3,8 @@ nCount NUMBER;
 v_sql LONG;
 
 begin
-SELECT count(*) into nCount FROM dba_tables where table_name = 'LOGI';
+SELECT count(*) into nCount FROM dba_tables 
+where table_name = 'LOGI' and owner = (select user from dual);
 IF(nCount <= 0)
 THEN
 v_sql:='CREATE TABLE LOGI(

0e4f247560cf3b5a0767cf51971a1c69ebd06023
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2018-06-16 12:50:21 +0200

    Conditional table creation. Now script can be executed multiple times.

--- logi.sql
+++ logi.sql
@@ -1,7 +1,19 @@
-CREATE TABLE logi(
+declare
+nCount NUMBER;
+v_sql LONG;
+
+begin
+SELECT count(*) into nCount FROM dba_tables where table_name = 'EMPLOYEE';
+IF(nCount <= 0)
+THEN
+v_sql:='CREATE TABLE logi(
   id number(4) GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
   tabela varchar2(50),
   uzytkownik varchar2(30),
   data date,
   operacja varchar2(6)
-);
+)';
+execute immediate v_sql;
+
+END IF;
+end;

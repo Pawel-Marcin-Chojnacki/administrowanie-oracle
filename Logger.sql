a78be0a87bdff586405f840ff63fed99a4cfa079
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2018-06-16 12:00:47 +0200

    Added trigger description.

--- Logger.sql
+++ Logger.sql
@@ -1,3 +1,5 @@
+-- This trigger logs on INSERT, DELETE and UPDATE events.
+-- It saves information about a table name, user and operation type that has triggered logger.
 create or replace trigger logger 
 after insert or update or delete 
 on hr.jobs
